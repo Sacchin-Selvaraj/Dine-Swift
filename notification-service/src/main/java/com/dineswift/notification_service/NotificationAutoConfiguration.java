f01845890dcb09fba822c4505b26267ba8a32bfc
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-09-20 13:19:27 +0530

    Notifcation Service

---
+++ notification-service/src/main/java/com/dineswift/notification_service/NotificationAutoConfiguration.java
@@ -0,0 +1,10 @@
+package com.dineswift.notification_service;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class NotificationAutoConfiguration {
+    public static void main(String[] args){
+
+    }
+}
